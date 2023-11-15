20bf10ba6cb4c87bdea39f4f797ff72ed27a11ec
Priyanka Haveri <Priyanka Haveri@LAPTOP-43UM0UT8>
2023-11-15 13:17:05 +0530

    first Commit

---
+++ src/tnsif/dayone/Hello.java
@@ -0,0 +1,18 @@
+package tnsif.dayone;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I");
+		System.out.println("Love");
+		System.out.println("Java");
+		
+		System.out.println("programming");
+		System.out.println("Language");
+		System.out.println("Thank you");
+		
+
+	}
+
+}
---
+++ src/tnsif/dayone/IdentifiersDemo.java
@@ -0,0 +1,19 @@
+package tnsif.dayone;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//invalid identifiers
+		//int class=13; //error because keyword cannot be ur identifier
+		//int num 2=13;//because we use spaces in identifiers
+		int $number=20;
+		System.out.println("value of the num variable is :"+$number);
+		
+		String studentName="Priyanka";
+		System.out.println("value is :"+studentName);
+		
+
+	}
+
+}
